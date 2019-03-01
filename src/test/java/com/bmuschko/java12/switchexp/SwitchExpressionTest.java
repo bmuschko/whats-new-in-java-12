d5ab2014a67053fda3fb83aea361e831ca31e6a9
Benjamin Muschko <benjamin.muschko@gmail.com>
2019-03-01 16:31:18 -0700

    first commit

---
+++ src/test/java/com/bmuschko/java12/switchexp/SwitchExpressionTest.java
@@ -0,0 +1,75 @@
+package com.bmuschko.java12.switchexp;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SwitchExpressionTest {
+    @DisplayName("Can use switch arrow syntax and avoid break statement")
+    @ParameterizedTest
+    @MethodSource("storageTypes")
+    void canUseSwitchArrowSyntaxToAvoidBreakStatement(StorageType storageType, String target) throws URISyntaxException {
+        URI result = switch (storageType) {
+            case LOCAL_FILE -> result = storageType.getTarget();
+            case CLOUD -> result = storageType.getTarget();
+            default -> throw new IllegalArgumentException("Unknown storage type");
+        };
+
+        assertEquals(new URI(target), result);
+    }
+
+    @DisplayName("Can use switch arrow syntax and break with return value")
+    @ParameterizedTest
+    @MethodSource("storageTypes")
+    void canUseSwitchArrowSyntaxAndReturnWithBreakStatement(StorageType storageType, String target) throws URISyntaxException {
+        URI result = switch (storageType) {
+            case LOCAL_FILE -> {
+                System.out.println("Handling local file storage type");
+                break storageType.getTarget();
+            }
+            case CLOUD -> {
+                System.out.println("Handling cloud storage type");
+                break storageType.getTarget();
+            }
+            default -> {
+                System.out.println("Fallthrough storage type");
+                throw new IllegalArgumentException("Unknown storage type");
+            }
+        };
+
+        assertEquals(new URI(target), result);
+    }
+
+    private static Stream<Arguments> storageTypes() {
+        return Stream.of(
+                Arguments.of(StorageType.LOCAL_FILE, "file://~/storage"),
+                Arguments.of(StorageType.CLOUD, "http://mycloud.com/data")
+        );
+    }
+
+    private enum StorageType {
+        LOCAL_FILE("file://~/storage"),
+        CLOUD("http://mycloud.com/data");
+
+        private final URI target;
+
+        StorageType(String target) {
+            try {
+                this.target = new URI(target);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public URI getTarget() {
+            return target;
+        }
+    }
+}
