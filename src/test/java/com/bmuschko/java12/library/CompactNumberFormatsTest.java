06b0dfcc69ee87c9941f5f9868ae230960a40a5e
Benjamin Muschko <benjamin.muschko@gmail.com>
2019-03-04 10:16:29 -0700

    Use final variable

--- src/test/java/com/bmuschko/java12/library/CompactNumberFormatsTest.java
+++ src/test/java/com/bmuschko/java12/library/CompactNumberFormatsTest.java
@@ -10,20 +10,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CompactNumberFormatsTest {
     @Test
     void canFormatNumbersInDifferentLocales() {
+        final Long twentyFiveThousand = 25000L;
         // US number formatting
         NumberFormat numberFormatUsShort
                 = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.SHORT);
         NumberFormat numberFormatUsLong
                 = NumberFormat.getCompactNumberInstance(Locale.US, NumberFormat.Style.LONG);
-        assertEquals(numberFormatUsShort.format(25000), "25K");
-        assertEquals(numberFormatUsLong.format(25000), "25 thousand");
+        assertEquals(numberFormatUsShort.format(twentyFiveThousand), "25K");
+        assertEquals(numberFormatUsLong.format(twentyFiveThousand), "25 thousand");
 
         // German number formatting
         NumberFormat numberFormatDeShort
                 = NumberFormat.getCompactNumberInstance(Locale.GERMANY, NumberFormat.Style.SHORT);
         NumberFormat numberFormatDeLong
                 = NumberFormat.getCompactNumberInstance(Locale.GERMANY, NumberFormat.Style.LONG);
-        assertEquals(numberFormatDeShort.format(25000), "25.000");
-        assertEquals(numberFormatDeLong.format(25000), "25 Tausend");
+        assertEquals(numberFormatDeShort.format(twentyFiveThousand), "25.000");
+        assertEquals(numberFormatDeLong.format(twentyFiveThousand), "25 Tausend");
     }
 }
