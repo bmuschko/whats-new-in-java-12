2b8af2e7f05d8df43973a02763d90216b2a4f586
Benjamin Muschko <benjamin.muschko@gmail.com>
2019-03-22 09:05:58 -0600

    Simply example

--- src/test/java/com/bmuschko/java12/library/StringEnhancementTest.java
+++ src/test/java/com/bmuschko/java12/library/StringEnhancementTest.java
@@ -2,6 +2,9 @@ package com.bmuschko.java12.library;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,40 +25,30 @@ public class StringEnhancementTest {
 
     @Test
     void canTransformString() {
-        StringBuilder formattedToDos = new StringBuilder();
-        List<ToDoItem> toDoItems = List.of(
-                new ToDoItem(true, "Buy milk"),
-                new ToDoItem(false, "File taxes"));
-        for (ToDoItem item : toDoItems) {
-            if (item.isChecked()) {
-                formattedToDos.append(item.getDescription()
-                        .transform(input -> "[ ] " + item.getDescription() + "\n")
-                        .transform(String::toString));
-            } else {
-                formattedToDos.append(item.getDescription()
-                        .transform(input -> "[X] " + item.getDescription() + "\n")
-                        .transform(String::toString));
-            }
+        List<String> urls = List.of(
+                "   http://google.com/",
+                "http://my.search.com?query=java server&page=1");
+        List<String> transformedUrls = new ArrayList<>();
+        for (String url : urls) {
+            String transformedUrl = url.transform(String::strip)
+                    .transform(URLCleaner::encodeQueryParams);
+            transformedUrls.add(transformedUrl);
         }
-        assertEquals("[ ] Buy milk\n" +
-                "[X] File taxes\n", formattedToDos.toString());
+        assertEquals(List.of("http://google.com/",
+                "http://my.search.com?query%3Djava+server%26page%3D1"), transformedUrls);
     }
 
-    private static class ToDoItem {
-        private final boolean checked;
-        private final String description;
-
-        public ToDoItem(boolean checked, String description) {
-            this.checked = checked;
-            this.description = description;
-        }
-
-        public boolean isChecked() {
-            return checked;
-        }
-
-        public String getDescription() {
-            return description;
+    private static class URLCleaner {
+        public static String encodeQueryParams(String rawURL) {
+            try {
+                if (rawURL.contains("?")) {
+                    String[] splitURL = rawURL.split("\\?");
+                    return splitURL[0] + "?" + URLEncoder.encode(splitURL[1], "UTF-8");
+                }
+                return rawURL;
+            } catch (UnsupportedEncodingException ex) {
+                throw new RuntimeException("UTF-8 not supported", ex);
+            }
         }
     }
 }
